76f1dd16a7b65490f7c6bf0baae222bc363410ee
HEOSHII <george.shiryaev@gmail.com>
2022-06-27 15:17:51 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import Header from "./components/Header";
-import Main from "./components/Main";
-import { useDispatch, useSelector } from "react-redux";
-import React, { useEffect } from "react";
-import { asyncGetProdutcs } from "./store/actions";
+import Header from './components/Header';
+import Main from './components/Main';
+import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { asyncGetProdutcs } from './store/actions';
 
 function App() {
   const dispatch = useDispatch();
-  const params = useSelector((state) => state.paramsReducer.params);
-  const isCardOpen = useSelector(
-    (state) => state.currentProductReducer.isCardOpen
-  );
+  const params = useSelector(state => state.paramsReducer.params);
+  const isCardOpen = useSelector(state => state.currentProductReducer.isCardOpen);
   if (isCardOpen) {
-    document.body.style.overflow = "hidden";
+    document.body.style.overflow = 'hidden';
   } else {
-    document.body.style.overflow = "auto";
+    document.body.style.overflow = 'auto';
   }
 
   console.log(params);
